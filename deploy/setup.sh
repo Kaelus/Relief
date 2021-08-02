353f0c1ea3d9b93b31ed9449df3797c8930d9b1a
Ben Kim <beomheyn.kim@gmail.com>
2021-08-02 16:12:55 +0900

    can figure out keystorefile and tracefilename

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
 if [ $# -lt 4 ]; then
-    echo "usage: setup.sh <num_server> <num_ycsb> <working_dir> <history_mode>"
+    echo "usage: setup.sh <num_server> <num_ycsb> <working_dir> <relief_dir> <history_mode>"
     exit 1
 fi
 
 num_server=$1
 num_ycsb=$2
 working_dir=$3
-history_mode=$4
+relief_dir=$4
+history_mode=$5
 #conf_dir=$working_dir/conf
 
 scriptdir=`dirname $0`
@@ -50,6 +51,8 @@ while [ $i -lt $num_server ]; do
 	mkdir -p $working_dir/$NODE_ID
 	cp -rf $scriptdir/reliefClient.conf-template $working_dir/$NODE_ID/reliefClient.conf
 	sed -i -e "1s|.*|srvip=127.0.0.1:$THIS_RELIEF_PORT|" $working_dir/$NODE_ID/reliefClient.conf
+	sed -i -e "3s|.*|keyStoreFile=$relief_dir/data/mykeystore.jks|" $working_dir/$NODE_ID/reliefClient.conf
+	sed -i -e "5s|.*|traceFileName=$working_dir/$NODE_ID/traceFile|" $working_dir/$NODE_ID/reliefClient.conf
 
 	j=`expr $j + 1`
     done
